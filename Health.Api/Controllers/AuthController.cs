0c2a40f5b0f66c4982923f52746ec98df8e73236
filip-matijevic <46181286+filip-matijevic@users.noreply.github.com>
2025-08-20 20:43:46 +0200

    trying out endpoint

---
+++ Health.Api/Controllers/AuthController.cs
@@ -0,0 +1,20 @@
+﻿using Microsoft.AspNetCore.Mvc;
+
+namespace Health.Api.Controllers;
+
+[ApiController]
+[Route("api/[controller]")]
+public class AuthController : ControllerBase
+{
+    [HttpGet("GetSomething")]
+    public IActionResult GetSomething()
+    {
+        return Ok("Something");
+    }
+
+    [HttpGet("GetSomethingElse")]
+    public IActionResult GetSomethingElse()
+    {
+        return Ok("Something Else");
+    }
+}
