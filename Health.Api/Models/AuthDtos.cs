029a01465a72ce400a27f4c41157f72b7805328e
filip-matijevic <46181286+filip-matijevic@users.noreply.github.com>
2025-08-22 15:46:05 +0200

    added the frontend project

--- Health.Api/Models/AuthDtos.cs
+++ Health.Api/Models/AuthDtos.cs
@@ -2,3 +2,5 @@
 
 public record RegisterRequestDto(string Username, string Password);
 public record LoginRequestDto(string Username, string Password);
+public record TokenResponseDto(string AccessTokej, string RefreshToken);
+public record RefreshRequestDto(Guid UserId, string RefreshToken);
