8c04f0afb2bfa51f1ceafa3eb4e07a64372867fd
filip-matijevic <46181286+filip-matijevic@users.noreply.github.com>
2025-09-15 19:15:08 +0200

    trying out update

--- health-web/src/components/views/NewMeasurementView.tsx
+++ health-web/src/components/views/NewMeasurementView.tsx
@@ -7,7 +7,7 @@ export default function NewMeasurementView({
   return (
     <div className="text-white h-50 flex flex-col justify-center text-center border-dashed border-1 rounded-xl m-2">
       a couple of input fields to create a new measurement. should input the name, short description. optional inputs could be 
-      time to reach the goal and goal.
+      time to reach the goal and goal. UPDATE TEST
     </div>
   );
 }
